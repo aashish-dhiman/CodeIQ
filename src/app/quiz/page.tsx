2658c5ca9720aeb4e240e47d5b8067cc4225024a
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-12-21 15:48:46 +0530

    added: prisma configuration

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -4,8 +4,9 @@ import React from "react";
 const Quiz = () => {
     return (
         <div>
-            <div>Quiz</div>
+            <div className="text-red-600">Quiz</div>
             <Button variant="outline">Button</Button>
+            
         </div>
     );
 };
